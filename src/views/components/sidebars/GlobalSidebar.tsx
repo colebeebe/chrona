4508759eb927c588b709de1580af20fe9dbb018e
outtaspacecode <cole.beebe@protonmail.com>
2026-05-28 22:47:45 -0400

    Created login prompt

--- src/views/components/sidebars/GlobalSidebar.tsx
+++ src/views/components/sidebars/GlobalSidebar.tsx
@@ -1,4 +1,5 @@
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';
 import { GoHome } from 'react-icons/go';
 import { CiCalendar } from 'react-icons/ci';
 import { VscAccount } from 'react-icons/vsc';
@@ -6,47 +7,113 @@ import { IoSettingsOutline } from 'react-icons/io5';
 import './GlobalSidebar.css';
 
 function GlobalSidebar() {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-    <div className="sidebar__global">
-      <nav>
-        <ul className="global__top-links">
-          <li>
-            <NavLink
-              to="/home"
-              className={({ isActive }) => (isActive ? 'active-link' : '')}
-              aria-label="home"
-            >
-              <GoHome size={30} strokeWidth={0.3} />
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/calendar"
-              className={({ isActive }) => (isActive ? 'active-link' : '')}
-              aria-label="calendar"
-            >
-              <CiCalendar size={30} strokeWidth={0.75} />
-            </NavLink>
-          </li>
-        </ul>
-        <ul className="global__bottom-links">
-          <li>
-            <button className="account-login-button">
-              <VscAccount size={30} />
-            </button>
-          </li>
-          <li>
-            <NavLink
-              to="/settings"
-              className={({ isActive }) => (isActive ? 'active-link' : '')}
-              aria-label="settings"
-            >
-              <IoSettingsOutline size={30} />
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    <>
+      <div className="sidebar__global">
+        <nav>
+          <ul className="global__top-links">
+            <li>
+              <NavLink
+                to="/home"
+                className={({ isActive }) => (isActive ? 'active-link' : '')}
+                aria-label="home"
+              >
+                <GoHome size={30} strokeWidth={0.3} />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/calendar"
+                className={({ isActive }) => (isActive ? 'active-link' : '')}
+                aria-label="calendar"
+              >
+                <CiCalendar size={30} strokeWidth={0.75} />
+              </NavLink>
+            </li>
+          </ul>
+          <ul className="global__bottom-links">
+            <li>
+              <button
+                className="account-login-button"
+                onClick={() => setShowLogin(true)}
+              >
+                <VscAccount size={30} />
+              </button>
+            </li>
+            <li>
+              <NavLink
+                to="/settings"
+                className={({ isActive }) => (isActive ? 'active-link' : '')}
+                aria-label="settings"
+              >
+                <IoSettingsOutline size={30} />
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {showLogin && (
+        <div
+          className="login-form__container"
+          onClick={() => setShowLogin(false)}
+        >
+          <form
+            action="/login"
+            method="POST"
+            className="login-form"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="login-form__section">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter email"
+                required
+              />
+            </div>
+
+            <div className="login-form__section">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter password"
+                minLength={8}
+                required
+              />
+              <div className="form-help-message">
+                Must be 8 characters long and have a number and special
+                character
+              </div>
+            </div>
+
+            <div className="option-buttons">
+              <button type="submit" className="btn btn-accent">
+                Log In
+              </button>
+              <button
+                className="btn"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setShowLogin(false);
+                }}
+              >
+                Cancel
+              </button>
+            </div>
+
+            <div className="create-account-message">
+              Don't have an account? <Link to="/">Create one</Link>.
+            </div>
+          </form>
+        </div>
+      )}
+    </>
   );
 }
 
